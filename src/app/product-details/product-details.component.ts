ce0fd12744be378a19fe7ce24bd0a3857c62948b
obieda01 <alakmo8@gmail.com>
2019-08-17 14:46:57 -0400

    1 file modified - Aug 17 work

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -14,9 +14,9 @@ export class ProductDetailsComponent implements OnInit {
   constructor(private route: ActivatedRoute,) { }
 
   ngOnInit() {
-     this.route.paramMap.subscribe(params => {
+  this.route.paramMap.subscribe(params => {
     this.product = products[+params.get('productId')];
   });
-  }
+}
 
 }
