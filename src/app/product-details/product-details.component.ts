eb59c998f87d2c6c00c9e1ac8a153c58b349e150
obieda01 <alakmo8@gmail.com>
2019-08-22 02:44:13 -0400

    1 file modified - Aug 22 work

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -12,9 +12,10 @@ import { CartService } from '../cart.service';
 export class ProductDetailsComponent implements OnInit {
   product;
 
-  constructor(private route: ActivatedRoute,) { }
+  constructor(private route: ActivatedRoute,private cartService: CartService) { }
 
   ngOnInit() {
+    
   this.route.paramMap.subscribe(params => {
     this.product = products[+params.get('productId')];
   });
