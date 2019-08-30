1491d5b87eaddcb838f36a2f2d4af2f043a63569
obieda01 <alakmo8@gmail.com>
2019-08-30 02:24:54 -0400

    1 file modified - Aug 30 work

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -24,6 +24,8 @@ import { ShippingComponent } from './shipping/shipping.component';
       { path: '', component: ProductListComponent },
       { path: 'products/:productId', component: ProductDetailsComponent },
       { path: 'cart', component: CartComponent },
+      { path: 'shipping', component: ShippingComponent },
+
     ])
   ],
   declarations: [
